99978c6760276342f102bb276f863d6e248ae176
AbdulRazaq1789 <usqarzi1789@gmail.com>
2026-02-23 16:23:21 +0430

    fixed env

--- app/config/settings.py
+++ app/config/settings.py
@@ -6,7 +6,7 @@ import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = env("SECRET_KEY", default="dev-secret-key-change-me")
+SECRET_KEY = env("SECRET_KEY", default="@y_a=h&_l_sn_2v!y91$")
 DEBUG = env("DEBUG", default=True, cast=bool)
 ALLOWED_HOSTS = env("ALLOWED_HOSTS", default="127.0.0.1,localhost").split(",")
 
@@ -65,11 +65,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(
+        env("DATABASE_URL", default="postgres://postgres:postgres@127.0.0.1:5432/hrpayroll"),
+        conn_max_age=60,
+    )
 }
 
 
